9e91e34b1ec1d86af98b3f7f13d7fc18dc56f921
Werner Schmid <wws@gmx.at>
2015-07-13 16:17:50 +0200

    added possiblity to copy S3 bucket to the  or to copy  to S3 bucket

--- run.sh
+++ run.sh
@@ -4,9 +4,26 @@ AWS_SECRET=${AWS_SECRET}
 BUCKET=${BUCKET}
 S3FS_OPTS=${S3FS_OPS}
 
+TARGET=${TARGET}
+SOURCE=${SOURCE}
+
 echo "${AWS_KEY}:${AWS_SECRET}" > /etc/passwd-s3fs
 chmod 0600 /etc/passwd-s3fs
 
 s3fs ${BUCKET} /s3bucket ${S3FS_OPTS}
 
+
+if [ ! -z "$TARGET" ]
+then
+cp -r /s3bucket/* $TARGET
+fi
+
+if [ ! -z "$SOURCE" ]
+then
+cp -r $SOURCE /s3bucket/
+fi
+
+if [ -z "$TARGET" ] && [ -z "$SOURCE" ]
+then
 sleep infinity
+fi
