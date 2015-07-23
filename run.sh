e1eede8f56877591571a64e6b0236df47d562123
Werner Schmid <wws@gmx.at>
2015-07-23 21:53:44 +0200

    added possibility to pass a command to the container with the env var S3CMD

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ S3FS_OPTS=${S3FS_OPS}
 
 TARGET=${TARGET}
 SOURCE=${SOURCE}
+S3CMD=${S3CMD}
 
 echo "${AWS_KEY}:${AWS_SECRET}" > /etc/passwd-s3fs
 chmod 0600 /etc/passwd-s3fs
@@ -30,10 +31,28 @@ echo "copy to S3 Bucket: $BUCKET ..."
 echo ""
 cp -v -r $SOURCE /s3bucket/
 echo ""
-echo "copy tp S3 Bucket: $BUCKET finished"
+echo "copy to S3 Bucket: $BUCKET finished"
 fi
 
-if [ -z "$TARGET" ] && [ -z "$SOURCE" ]
+if [ ! -z "$S3CMD" ]
+then
+    echo ""
+    echo "executing S3CMD: $S3CMD ..."
+    echo ""
+    $S3CMD
+    if [ $? -eq 0 ]
+    then
+	echo ""
+	echo "execution of S3CMD: $S3CMD finished"
+    else
+	echo "FAILED"
+	echo "Could not execute: $S3CMD"
+	exit 1
+    fi
+fi
+
+
+if [ -z "$TARGET" ] && [ -z "$SOURCE" ] && [ -z "$S3CMD" ]
 then
 sleep infinity
 fi
